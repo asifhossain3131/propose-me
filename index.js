29d93650e110fd5fd704f120f4febf464e39a4e0
David Davila <daviladavid786@gmail.com>
2023-02-03 23:15:37 +0600

    special commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-1').addEventListener('click', function(){
+document.getElementById('msg').innerText='I LOVE YOU'
+})
+document.getElementById('btn-2').addEventListener('click', function(){
+document.getElementById('msg').innerText='YOU ARE VERY SPECIAL'
+})
