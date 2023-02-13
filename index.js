3fee0aa70d2450336f4e932d366ea954f837e255
David Davila <daviladavid786@gmail.com>
2023-02-13 20:56:01 +0600

    audio added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 document.getElementById('btn-1').addEventListener('click', function(){
 document.getElementById('msg').innerText='I LOVE YOU'
+const music=document.getElementById('music');
+music.play();
 })
 document.getElementById('btn-2').addEventListener('click', function(){
 document.getElementById('msg').innerText='YOU ARE VERY SPECIAL'
