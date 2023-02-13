96be3a60589f87853b27c2433479493113e1a025
David Davila <daviladavid786@gmail.com>
2023-02-13 21:09:41 +0600

    audio new added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 document.getElementById('btn-1').addEventListener('click', function(){
 document.getElementById('msg').innerText='I LOVE YOU'
-const music=document.getElementById('music');
-music.play();
+const music1=document.getElementById('music1');
+music1.play();
 })
 document.getElementById('btn-2').addEventListener('click', function(){
 document.getElementById('msg').innerText='YOU ARE VERY SPECIAL'
+const music2=document.getElementById('music2');
+music2.play();
 })
